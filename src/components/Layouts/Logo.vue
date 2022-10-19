d1a24adc8382177a0630cc0ec06db93493198d26
Fahriz Dimasqy <1197050037@student.uinsgd.ac.id>
2022-10-20 00:59:43 +0700

    convert home from html to vue

---
+++ src/components/Layouts/Logo.vue
@@ -0,0 +1,12 @@
+<script setup>
+import { RouterLink } from "vue-router";
+</script>
+<template>
+  <RouterLink to="/" class="flex items-center">
+    <img
+      src="@/assets/img/logo.png"
+      class="h-8 ml-3 sm:ml-0 sm:h-8 w-50 sw:50"
+      alt="Flowbite Logo"
+    />
+  </RouterLink>
+</template>
