9d0aca5000618927de22bc9d15d7eac25e3fed74
Fahriz Dimasqy <1197050037@student.uinsgd.ac.id>
2022-10-22 20:49:03 +0700

    change database connection

--- src/router/index.js
+++ src/router/index.js
@@ -19,6 +19,11 @@ const router = createRouter({
       name: "register",
       component: () => import("../views/RegisterView.vue"),
     },
+    {
+      path: "/detail",
+      name: "detail",
+      component: () => import("../views/DetailView.vue"),
+    },
   ],
 });
 
